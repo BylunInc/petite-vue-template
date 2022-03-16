5ad5011112b6b52bba9114737082654c53d836f2
Bylun <88210532+BylunInc@users.noreply.github.com>
2022-03-16 01:24:57 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      colors: {
+        vue_green: '#42b883'
+      }
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
+}
